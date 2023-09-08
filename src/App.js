9c5ee73a15f6149c2c4b2fcaad19a05dd7841904
Freebird <bo.salinas12@gmail.com>
2023-09-07 20:35:30 -0500

    added file directories and hello world to render on th page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Project from "./components/Project";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+export default function App() {
+    return (
+        <div className='portfolio-app'>
+            <Nav />,
+            <Header />,
+            <Project />,
+            <Footer />
+        </div>
+    )
+}
