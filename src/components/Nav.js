4336bffc3538a70825982a38a99b0918736a4f70
Freebird <bo.salinas12@gmail.com>
2023-09-10 15:05:09 -0500

    nav items

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Nav() {
     return (
-        <h3>Nav components</h3>
+        <nav>
+         <ul>
+            <li>About Me</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+            <li>Resume</li>
+         </ul>
+        </nav>
     )
 }
