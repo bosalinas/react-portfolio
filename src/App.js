d972624b10b883efd1e98a4ac725f7fb3bb8e298
Freebird <bo.salinas12@gmail.com>
2023-09-11 08:43:59 -0500

    added coditional rendering and conditional styling to project and contact

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Project from "./components/Project";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Contact from "./components/Contact";
+import React, { useState } from 'react';
 
 export default function App() {
+    const [section, setSection] = useState('About Me');
+
     return (
         <div className='portfolio-app'>
-            <Nav />,
-            <Header />,
-            <Project />,
+            <Nav section={section} setSection={setSection}/>
+            {section==="Portfolio"?<Project />:""}
+            {section==="Contact"?<Contact />:""}
             <Footer />
         </div>
     )
 }
+
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return (
-        <h3>Header components</h3>
-    )
-}
