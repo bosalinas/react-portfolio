08e6413784c744db9badf00355e13aebd5b960ed
Freebird <bo.salinas12@gmail.com>
2023-09-13 08:11:13 -0500

    updated return on project.js and portfolio.js. styles and card info are rendering now

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+import "./Project.css"
+import { Button } from 'reactstrap';
 
 export default function Project(project) {
     let { projectName, link } = project;
-    // let img = props.img;
+
     return (
-            <Card style={{ width: '18rem' }}>
-                <img/>
-                <CardBody>
-                    <CardTitle tag="h5">
-                        {projectName}
-                    </CardTitle>
-                    <CardText>
-                        Some quick example text to build on the card title and make up the bulk of the card‘s content.
-                    </CardText>
-                    <Button>{link}</Button>
-                </CardBody>
-            </Card>
+            <div className='project-card'>
+                <img src='' />
+                <h3>
+                    {projectName}
+                </h3>
+                <div className='info-group'>
+                    <p>Text for the project</p>
+                </div>
+                <a href={link}><Button className='btn'>View</Button></a>
+            </div>
     )
 }
